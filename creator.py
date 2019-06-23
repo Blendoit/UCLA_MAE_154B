70116a396caa2ae02b857f751a4df31886592795
Marius Peter <blendoit@gmail.com>
2019-06-23 12:21:32 -0700

    Airfoil docstring

--- creator.py
+++ creator.py
@@ -112,6 +112,9 @@ class Airfoil(Coordinates):
     for the x- and z-coordinates. The coordinates start at
     the leading edge, travel over the airfoil's upper edge,
     then loops back to the leading edge via the lower edge.
+    This method was chosen for easier future exports
+    to 3D CAD packages like SolidWorks, which can import
+    geometry as coordinates written in a CSV file.
     '''
 
     def __init__(self):
